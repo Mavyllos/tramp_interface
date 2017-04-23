fecaf7cb596f235f47a21bf3cf3bdd030e470123
Erica Rottman <erica7650@gmail.com>
2017-04-23 14:07:28 -0700

    able to dsiplay json

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,42 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../db/connection.js')
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+const db = require('../db/connection.js')
+const ownersURL = "http://young-anchorage-23408.herokuapp.com/owners";
+const dogsURL="http://young-anchorage-23408.herokuapp.com/dogs";
+const walkersURL="http://lit-garden-29083.herokuapp.com/walkers";
+const companiesURL="http://lit-garden-29083.herokuapp.com/companies";
+const rp = require('request-promise')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/owners', (req, res, next) => {
+  rp(ownersURL)
+  .then(owners => {
+    res.json({owners})
+  })
+})
+
+router.get('/dogs', (req, res, next) => {
+  rp(dogsURL)
+  .then(dogs => {
+    res.json({dogs})
+  })
+})
+
+router.get('/walkers', (req, res, next) => {
+  rp(walkersURL)
+  .then(walkers => {
+    res.json({walkers})
+  })
+})
+
+router.get('/companies', (req, res, next) => {
+  rp(companiesURL)
+  .then(companies => {
+    res.json({companies})
+  })
+})
+
+
+
 
 module.exports = router;
