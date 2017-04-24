c3cb066061d9377ede7e2f51935f229ba9a4ae40
Erica Rottman <erica7650@gmail.com>
2017-04-24 10:23:51 -0700

    finished getResources route function

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,33 @@ const walkersURL="http://lit-garden-29083.herokuapp.com/walkers";
 const companiesURL="http://lit-garden-29083.herokuapp.com/companies";
 const rp = require('request-promise')
 
-router.get('/owners', (req, res, next) => {
-  rp(ownersURL)
-  .then(owners => {
-    res.json({owners})
-  })
-})
+router.get('/owners', getResouces(ownersURL));
+router.get('/walkers', getResouces(walkersURL));
+router.get('/dogs', getResouces(dogsURL));
+router.get('/companies', getResouces(companiesURL));
+
+
+function getResouces(resource) {
+  return function (req, res) {
+    rp(resource)
+      .then(result => {
+        let resultArray = JSON.parse(result);
+        console.log(resultArray);
+        res.render('index', {resultArray})
+    })
+  }
+}
+
+
+
+// router.get('/owners', (req, res, next) => {
+//   rp(ownersURL)
+//   .then(owners => {
+//     let ownersArray = JSON.parse(owners);
+//     console.log(ownersArray);
+//     res.render('index', {ownersArray})
+//   })
+// })
 
 router.get('/dogs', (req, res, next) => {
   rp(dogsURL)
