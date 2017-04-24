70d3b4ff94868be5b670e4ff81ac0133627d791b
Erica Rottman <erica7650@gmail.com>
2017-04-24 14:05:19 -0700

    complete delete routes

--- routes/index.js
+++ routes/index.js
@@ -23,14 +23,23 @@ router.post('/walkers', createResource(walkersURL));
 router.post('/dogs', createResource(dogsURL));
 router.post('/companies', createResource(companiesURL));
 
+router.put('/owners/:id', updateResource(ownersURL));
+router.put('/walkers/:id', updateResource(walkersURL));
+router.put('/dogs/:id', updateResource(dogsURL));
+router.put('/companies/:id', updateResource(companiesURL));
+
+router.delete('/owners/:id', deleteResource(ownersURL));
+router.delete('/walkers/:id', deleteResource(walkersURL));
+router.delete('/dogs/:id', deleteResource(dogsURL));
+router.delete('/companies/:id', deleteResource(companiesURL));
+
 
 function getResouces(resource) {
   return function (req, res) {
     rp(resource)
       .then(result => {
         let resultArray = JSON.parse(result);
-        console.log(resultArray);
-        res.render('index', {resultArray})
+        res.render('indexData', {resultArray})
     })
   }
 }
@@ -39,12 +48,11 @@ function getResouces(resource) {
 function getFilterID(resource) {
   return function (req, res) {
     let id = req.params.id;
-    let request = `${resource}${'/'}${id}`
+    let request = `${resource}${'/'}${id}`;
     rp(request)
     .then(result => {
       let resultArray = JSON.parse(result);
-      console.log(resultArray);
-      res.render('show', {
+      res.render('showData', {
         result: resultArray
       })
     })
@@ -81,7 +89,7 @@ function createResource(resource) {
       image: req.body.image,
     }
     function clean(create) {
-      for (var propName in create) {
+      for (let propName in create) {
         if (create[propName] === null || create[propName] === undefined) {
           delete create[propName];
         }
@@ -95,50 +103,83 @@ function createResource(resource) {
               body: create,
               json: true
             };
-      console.log(request);
     rp(request)
     .then(result => {
-      // let resultArray = JSON.parse(result);
-      console.log(result);
-      res.render('index', {result})
+      res.render('indexData', {result})
     })
   }
 }
 
 
+function updateResource(resource) {
+  return function (req,res) {
+    let id = req.params.id
+    let update = {
+      owner_id: req.body.owner_id,
+      walker_id: req.body.walker_id,
+      name: req.body.name,
+      description: req.body.description,
+      logo_img: req.body.logo_img,
+      url: req.body.url,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      address_line_1: req.body.address_line_1,
+      address_line_2: req.body.address_line_2,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip ,
+      phone_number: req.body.phone_number,
+      email_address: req.body.email_address,
+      user_id: req.body.user_id,
+      breed: req.body.breed,
+      size: req.body.size,
+      allergies: req.body.allergies,
+      kids: req.body.kids,
+      other_dogs: req.body.other_dogs,
+      other_people: req.body.other_people,
+      fav_treat: req.body.fav_treat,
+      bio: req.body.bio,
+      image: req.body.image,
+    }
+    function clean(update) {
+      for (let propName in update) {
+        if (update[propName] === null || update[propName] === undefined) {
+          delete update[propName];
+        }
+      }
+    }
+    clean(update)
+
+    let request = {
+              method: 'PUT',
+              uri: `${resource}${'/'}${id}`,
+              body: update,
+              json: true
+            };
+    rp(request)
+    .then(result => {
+      res.render('indexData', {result})
+    })
+  }
+}
 
 
-// router.get('/dogs', (req, res, next) => {
-//   rp(dogsURL)
-//   .then(dogs => {
-//     res.json({dogs})
-//   })
-// })
-//
-// router.get('/walkers', (req, res, next) => {
-//   rp(walkersURL)
-//   .then(walkers => {
-//     res.json({walkers})
-//   })
-// })
-//
-// router.get('/companies', (req, res, next) => {
-//   rp(companiesURL)
-//   .then(companies => {
-//     res.json({companies})
-//   })
-// })
-//
+function deleteResource(resource) {
+  return function (req,res) {
+    let id = req.params.id
 
+    let request = {
+              method: 'DELETE',
+              uri: `${resource}${'/'}${id}`,
+              json: true
+            };
+    rp(request)
+    .then(result => {
+      res.render('indexData', {result})
+    })
+  }
+}
 
-// router.get('/owners', (req, res, next) => {
-//   rp(ownersURL)
-//   .then(owners => {
-//     let ownersArray = JSON.parse(owners);
-//     console.log(ownersArray);
-//     res.render('index', {ownersArray})
-//   })
-// })
 
 
 module.exports = router;
